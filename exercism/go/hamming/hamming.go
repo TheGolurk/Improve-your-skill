b559e378ba72035faf7ac87afe43fb125417a314
Christian Hernandez Najera <chrismatla68@gmail.com>
2019-12-21 21:06:23 -0600

    Complete hamming

--- exercism/go/hamming/hamming.go
+++ exercism/go/hamming/hamming.go
@@ -1,17 +1,19 @@
 // Package hamming is a package to implement the calculation of dna
 package hamming
 
+import "errors"
+
 // Distance calculates the hamming distance
 func Distance(a, b string) (dis int, err error) {
 	if len(a) != len(b) {
-		return 0, nil
+		return 0, errors.New("Not Equal length")
 	}
 
-	dna := make(map[int]byte)
-
-	for index := 0; index <= len(a)+len(b); {
-		dna[index] = a[index]
+	for index := 0; index <= len(a)-1; index++ {
+		if a[index] != b[index] {
+			dis++
+		}
 	}
-	
+
 	return
 }

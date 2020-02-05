086115e9aaf7e213a34cb52b351ffca5b1d2a764
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-02-05 15:51:04 -0600

    added func

--- exercism/go/scrabble-score/scrabble_score.go
+++ exercism/go/scrabble-score/scrabble_score.go
@@ -1,1 +1,8 @@
+//Package scrabble it's a package to calculate the score of a word
 package scrabble
+
+//Score calculates the score of a word
+func Score(word string) (points int) {
+
+	return points
+}

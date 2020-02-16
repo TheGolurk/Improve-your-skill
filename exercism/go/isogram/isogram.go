9aabf7c4941115d99a4b797db28107fb4691f615
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-02-16 14:25:31 -0600

    added isIsogram func

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -1,1 +1,8 @@
+// Package isogram determinates if a word is an isogram
 package isogram
+
+// IsIsogram is a function to know if a word is an isogram
+func IsIsogram(word string) bool {
+
+	return true
+}

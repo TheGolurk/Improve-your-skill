8ffc279249b578f5e81158471ec6972ba2c55f8a
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-02-26 12:48:50 -0600

    some code

--- exercism/go/difference-of-squares/difference_of_squares.go
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,19 @@
 package diffsquares
+
+// SquareOfSum sum some natural numbers
+func SquareOfSum(number int) int {
+
+	return 0
+}
+
+// SumOfSquares sum of square of some numbers
+func SumOfSquares(number int) int {
+
+	return 0
+}
+
+// Difference between the square of the sum and the sum of the squares
+func Difference(number int) int {
+
+	return 0
+}

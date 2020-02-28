2079bea75f9512a4aa529ad0d02713a914f6dd7f
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-02-27 20:58:59 -0600

    new algorithm

--- exercism/go/difference-of-squares/difference_of_squares.go
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -4,33 +4,21 @@ package diffsquares
 // SquareOfSum sum some natural numbers
 func SquareOfSum(number int) (result int) {
 
-	for i := 1; i <= number; i++ {
-		result += i
-	}
-	result *= result
+	result = (number * (number + 1)) / 2
 
-	return result
+	return result * result
 }
 
 // SumOfSquares sum of square of some numbers
 func SumOfSquares(number int) (result int) {
 
-	for i := 1; i <= number; i++ {
-		result += i * i
-	}
+	result = (number * (number + 1) * ((number * 2) + 1)) / 6
 
 	return result
 }
 
 // Difference between the square of the sum and the sum of the squares
 func Difference(number int) int {
-	var resultSquareOfSum, resultSumOfSquare int
 
-	for i := 1; i <= number; i++ {
-		resultSquareOfSum += i
-		resultSumOfSquare += i * i
-	}
-	resultSquareOfSum *= resultSquareOfSum
-
-	return resultSquareOfSum - resultSumOfSquare
+	return SquareOfSum(number) - SumOfSquares(number)
 }

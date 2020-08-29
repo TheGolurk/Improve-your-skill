0cc7a38be5474cd01aefb02f35207cb2e7077317
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-08-28 23:23:30 -0500

    refactor fizz buzz

--- AlgorithmsNFPSolutions/Chapter1/fizzBuzz.js
+++ AlgorithmsNFPSolutions/Chapter1/fizzBuzz.js
@@ -1,10 +1,11 @@
 'use strict';
 
-let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,20,21,22,23,24,25,26,27];
 
 for (let num of number) {
 
-	if (num % 3 == 0 && num % 5 == 0) {
+	// (num % 3 == 0 && num % 5 == 0) Not Optimized
+	if (num % 15 == 0) {
 		console.log(`${num} fizz buzz`);
 	} else if (num % 3 == 0) {
 		console.log(`${num} fizz`);
